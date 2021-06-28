49eb6c0a0d258723358fdebca30b29fe7ef819a4
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-06-28 15:16:02 -0300

    iniciado componente SearchBar com as props propostas e implementado a validação do propTypes de cada props recebida de acordo com o tipo passado no requisito

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 
-import MovieList from './MovieList';
-import SearchBar from './SearchBar';
-import AddMovie from './AddMovie';
+// import MovieList from './MovieList';
+// import SearchBar from './SearchBar';
+// import AddMovie from './AddMovie';
 
-class MovieLibrary extends Component {
-  render() {
-    const { movies } = this.props;
-    return (
-      <div>
-        <h2> My awesome movie library </h2>
-        <SearchBar />
-        <MovieList movies={ movies } />
-        <AddMovie />
-      </div>
-    );
-  }
-}
+// class MovieLibrary extends Component {
+//   render() {
+//     const { movies } = this.props;
+//     return (
+//       <div>
+//         <h2> My awesome movie library </h2>
+//         <SearchBar />
+//         <MovieList movies={ movies } />
+//         <AddMovie />
+//       </div>
+//     );
+//   }
+// }
 
-export default MovieLibrary;
+// export default MovieLibrary;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,25 @@
-// implement SearchBar component here
+import React, { Component } from 'react';
+import { bool, func, string } from 'prop-types';
+
+class SearchBar extends Component {
+  render() {
+    const {
+      searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre,
+      onSelectedGenreChange,
+    } = this.props;
+    return (
+      <form />
+    );
+  }
+}
+
+SearchBar.propTypes = {
+  searchText: string.isRequired,
+  onSearchTextChange: func.isRequired,
+  bookmarkedOnly: bool.isRequired,
+  onBookmarkedChange: func.isRequired,
+  selectedGenre: string.isRequired,
+  onSelectedGenreChange: func.isRequired,
+};
+
+export default SearchBar;
