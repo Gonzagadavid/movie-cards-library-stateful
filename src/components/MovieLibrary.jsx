90e9bcb0100ab5c60c7e39aa5ff741d91949da19
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-06-28 14:35:32 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,21 @@
-// implement MovieLibrary component here
+import React, { Component } from 'react';
+
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
+
+class MovieLibrary extends Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div>
+        <h2> My awesome movie library </h2>
+        <SearchBar />
+        <MovieList movies={ movies } />
+        <AddMovie />
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
